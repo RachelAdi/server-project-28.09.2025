791ad5e3c7751d18b216ab72cb7da824c9606b7d
RachelAdi <r0548571737@gmail.com>
2025-09-29 10:00:50 +0300

    level 2

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const bookRouter = require("./router/bookRouter");
 
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello! Server is running ✅");
+});
+
 app.use("/books", bookRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:3000`);
 });
